8444bc92927770203fe433b3bb4bef27d2adc659
Mhijazi16 <gultimate0@gmail.com>
2023-07-21 12:08:41 +0300

    Domain Hotfix : create AddBook() & AddTransaction() methods to patrion.

--- Library/Library.Domain/Patrion/Aggregate/Patrion.cs
+++ Library/Library.Domain/Patrion/Aggregate/Patrion.cs
@@ -1,9 +1,42 @@
+using System.Net.Mail;
+
 namespace Library.Domain.Patrion.Aggregate;
 using Book.Aggregate;
 using Common.Transaction;
 
 public class Patrion
 {
+    public string FirstName{ get; private set; }
+    public string LastName{ get; private set; }
+    public string Email { get; private set; }
     public HashSet<Book>? Books { get; private set; } = null;
     public HashSet<PatrionTransaction>? Transactions { get; private set; } = null;
+    
+    private Patrion(){}
+
+    public Patrion(string first,string last, string email)
+    {
+        FirstName = first;
+        LastName = last;
+        Email = email;
+    }
+
+    public void AddBook(Book book)
+    {
+        if (Books is null)
+        {
+            Books = new HashSet<Book>();
+        }
+
+        Books.Add(book);
+    }
+    public void AddTransaction(PatrionTransaction transaction)
+     {
+         if (Transactions is null)
+         {
+             Transactions = new HashSet<PatrionTransaction>();
+         }
+
+         Transactions.Add(transaction);
+     }   
 }
