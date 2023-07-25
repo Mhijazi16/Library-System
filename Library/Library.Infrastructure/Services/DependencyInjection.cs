e414cfda3318f36803a35282f37e544bc2b48d03
Mhijazi16 <gultimate0@gmail.com>
2023-07-25 13:39:07 +0300

    Infrastructure : Register DbContext in the dependency Injection container

---
+++ Library/Library.Infrastructure/Services/DependencyInjection.cs
@@ -0,0 +1,17 @@
+using Library.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Library.Infrastructure.Services;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence
+        (this IServiceCollection services)
+    {
+        services.AddDbContext<LibraryDbContext>(options => 
+            options.UseSqlServer("Server=localhost; Database=Library; User=sa; Password=Hijazi123; TrustServerCertificate=True"));
+
+        return services;
+    }
+}
