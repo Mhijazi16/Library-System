0646a8fb2e8c6b52454afe4ddec61759514cc4dc
Mhijazi16 <gultimate0@gmail.com>
2023-07-25 12:07:54 +0300

    Infrastructure : add all Foreign Keys and Navigation Properties.

--- Library/Library.Domain/Book/Aggregate/Book.cs
+++ Library/Library.Domain/Book/Aggregate/Book.cs
@@ -1,21 +1,26 @@
 using Library.Domain.Book.Entities;
 using Library.Domain.Book.Enums;
 using Library.Domain.Book.Value_Object;
-using Library.Domain.Common.Transaction;
 using Library.Domain.Common.Transaction.Aggregate;
 
 namespace Library.Domain.Book.Aggregate;
 
 public class Book
 {
+
    public Guid Id { get; private set; }
-   public Guid AuthorId { get; private set; }
    public String Title { get; set; } 
+   public String Description { get; set; }
    public Genre Genre { get; set; }
    public Status Status { get; internal set; } = Status.Available;
-   public HashSet<Transaction>? TransactionHistory { get; private set; } = null;
-   public List<Review>? BookReviews { get; private set; } = null;
-   
+   private HashSet<Transaction>? _transactionHistory  = null;
+   private List<Review>? _bookReviews = null;
+    
+   //Navigation Properties & Foreign Keys  
+   public Guid AuthorId { get; private set; }
+   public IReadOnlyList<Transaction>? BookTransactions => _transactionHistory.ToList(); 
+   public IReadOnlyList<Review>? BookReviews => _bookReviews.ToList(); 
+
    private Book(){}
 
    public static Book Create(String title, Genre genre, Guid authorId)
@@ -31,21 +36,15 @@ public class Book
 
    public void AddTransaction(Transaction transaction)
    {
-       if (TransactionHistory is null)
-       {
-           TransactionHistory = new HashSet<Transaction>();
-       }
+       _transactionHistory ??= new HashSet<Transaction>();
 
-       TransactionHistory.Add(transaction);
+       _transactionHistory.Add(transaction);
    }
 
    public void AddReview(Review review)
    {
-       if (BookReviews is null)
-       {
-           BookReviews = new List<Review>();
-       }
-       
-       BookReviews.Add(review);
+       _bookReviews ??= new List<Review>();
+
+       _bookReviews.Add(review);
    }
 }
