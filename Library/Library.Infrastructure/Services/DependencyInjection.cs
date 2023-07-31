e8b518ca85806206aac17a9bab5e41633550e9a8
Mhijazi16 <gultimate0@gmail.com>
2023-07-31 13:53:40 +0300

    Application : Injecting The repositories in Depndency Injection Container.

--- Library/Library.Infrastructure/Services/DependencyInjection.cs
+++ Library/Library.Infrastructure/Services/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using Library.Application.Interfaces;
 using Library.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +13,8 @@ public static class DependencyInjection
         services.AddDbContext<LibraryDbContext>(options => 
             options.UseSqlServer("Server=localhost; Database=Library; User=sa; Password=Hijazi123; TrustServerCertificate=True"));
 
+        services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+        
         return services;
     }
 }
